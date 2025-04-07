fd0fb801f0c382b72d9e72886f9b50ff75ecb31a
Abhishek Singh <abhisheksinghdkm143@gmail.com>
2025-04-07 21:35:04 +0530

    instruction

--- frontend/src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ frontend/src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -5,10 +5,8 @@ import { Pie } from "react-chartjs-2"
 Chart.register(...registerables)
 
 export default function InstructorChart({ courses }) {
-  // State to keep track of the currently selected chart
   const [currChart, setCurrChart] = useState("students")
 
-  // Function to generate random colors for the chart
   const generateRandomColors = (numColors) => {
     const colors = []
     for (let i = 0; i < numColors; i++) {
@@ -20,7 +18,6 @@ export default function InstructorChart({ courses }) {
     return colors
   }
 
-  // Data for the chart displaying student information
   const chartDataStudents = {
     labels: courses.map((course) => course.courseName),
     datasets: [
@@ -31,7 +28,6 @@ export default function InstructorChart({ courses }) {
     ],
   }
 
-  // Data for the chart displaying income information
   const chartIncomeData = {
     labels: courses.map((course) => course.courseName),
     datasets: [
@@ -42,9 +38,9 @@ export default function InstructorChart({ courses }) {
     ],
   }
 
-  // Options for the chart
   const options = {
     maintainAspectRatio: false,
+    responsive: true,
   }
 
   return (
@@ -52,31 +48,29 @@ export default function InstructorChart({ courses }) {
       <p className="text-lg font-bold text-richblack-5">Visualize</p>
 
       <div className="space-x-4 font-semibold">
-        {/* Button to switch to the "students" chart */}
         <button
           onClick={() => setCurrChart("students")}
-          className={`rounded-sm p-1 px-3 transition-all duration-200 ${currChart === "students"
-            ? "bg-richblack-700 text-yellow-50"
-            : "text-yellow-400"
-            }`}
+          className={`rounded-sm p-1 px-3 transition-all duration-200 ${
+            currChart === "students"
+              ? "bg-richblack-700 text-yellow-50"
+              : "text-yellow-400"
+          }`}
         >
           Students
         </button>
-
-        {/* Button to switch to the "income" chart */}
         <button
           onClick={() => setCurrChart("income")}
-          className={`rounded-sm p-1 px-3 transition-all duration-200 ${currChart === "income"
-            ? "bg-richblack-700 text-yellow-50"
-            : "text-yellow-400"
-            }`}
+          className={`rounded-sm p-1 px-3 transition-all duration-200 ${
+            currChart === "income"
+              ? "bg-richblack-700 text-yellow-50"
+              : "text-yellow-400"
+          }`}
         >
           Income
         </button>
       </div>
 
-      <div className="relative mx-auto aspect-square h-full w-full">
-        {/* Render the Pie chart based on the selected chart */}
+      <div className="relative mx-auto aspect-square h-64 w-full sm:h-80">
         <Pie
           data={currChart === "students" ? chartDataStudents : chartIncomeData}
           options={options}
